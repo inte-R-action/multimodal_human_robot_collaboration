bug fix shimmer code, new classifier w/ scaling

sam_nodes/scripts/IMU_collator.py
@@ -73,17 +73,15 @@ pos = np.arange(len(CATEGORIES))
 
 def collate_imu_seq(imu_state_hist, imu_pred_hist):
     # 'Dilation' filter to remove single erroneous predictions
-    if np.shape(imu_state_hist)[0] >= 3:
+    if np.shape(imu_state_hist)[0] >= 4:
         if (imu_state_hist[-1, 0] == imu_state_hist[-3, 0]):# & (imu_state_hist[-2, 1] <= 0.00000000001): # WHAT IS THIS DOING???
             imu_state_hist[-2, 0] = imu_state_hist[-1, 0] #NEED TO CHECK CONFIDENCES HERE
 
         # Group predictions of same type together
         if imu_state_hist[-2, 0] == imu_state_hist[-3, 0]:
             i = np.where(imu_pred_hist[:, -1]==imu_state_hist[-3, 2])[0][0] #Get index where action starts
-            #i = i[0,0]
             imu_state_hist[-2, 1] = np.mean(imu_pred_hist[i:-1, int(imu_state_hist[-2, 0])].astype(float)) #Not convinced about this mean
-            imu_state_hist[-2, 2] = imu_state_hist[-3, 2]
-            imu_state_hist[-2, 3] = imu_pred_hist[i, -1]
+            imu_state_hist[-2, 2] = imu_state_hist[-3, 2] # set start time
             imu_state_hist = np.delete(imu_state_hist, -3, 0)
         else:
             # Can publish new episode to sql
@@ -91,11 +89,11 @@ def collate_imu_seq(imu_state_hist, imu_pred_hist):
             date = datetime.date.today()
             start_t = imu_state_hist[-3, 2]
             end_t = imu_state_hist[-3, 3]
-            dur = datetime.datetime.combine(datetime.date.today(), end_t) - datetime.datetime.combine(datetime.date.today(), start_t)
+            dur = end_t - start_t #datetime.datetime.combine(datetime.date.min, end_t) - datetime.datetime.combine(datetime.date.min, start_t)
             #table:str(table name), columns:[str(column name),], data:[('data1', data2, ),]
             db.insert_data_list("Episodes", 
             ["date", "start_t", "end_t", "duration", "user_id", "hand", "capability", "task_id"], 
-            [(date, start_t, end_t, dur, 0, "R", CATEGORIES[imu_state_hist[-3, 0]], 0)])
+            [(date, start_t, end_t, dur, 0, "R", CATEGORIES[int(imu_state_hist[-3, 0])], 0)])
 
         #     imu_pred_hist = np.array(imu_pred_hist[-1, :])
         #     imu_pred_hist = np.reshape(imu_pred_hist, (1, 5))

sam_nodes/scripts/imu_classifier.py
@@ -10,7 +10,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 plt.ion()
 CATEGORIES = ['AllenKeyIn', 'AllenKeyOut', 'ScrewingIn', 'ScrewingOut', 'Null']
 pos = np.arange(len(CATEGORIES))
-model = load_model(f'{dir_path}/IMUmodel.h5')
+model = load_model(f'{dir_path}/channel_scaling_1.h5')
 model.summary()
 
 

sam_nodes/scripts/shimmerBase.py
@@ -21,6 +21,7 @@ import shlex
 from imu_classifier import classify_data
 from diagnostic_msgs.msg import KeyValue
 from pub_classes import diag_class, act_class
+import csv
 
 IMU_MSGS = ['ERROR', 'Ready', 'Unknown', 'Shutdown', 'Starting', 'Connecting', 'Initialising']
 IMU_SYS_MSGS = ['ERROR', 'Ready', 'Setting Up']
@@ -60,7 +61,7 @@ frame_id = f'shimmerBase {args.user_name} {args.user_id} node'
 
 # Shimmer sensor connection params
 serialports = ['/dev/rfcomm0', '/dev/rfcomm1', '/dev/rfcomm2']
-POSITIONS = ['Hand', 'Wrist', 'ARM']
+POSITIONS = ['Hand', 'Wrist', 'Arm']
 SHIM_IDs = ['F2:AF:44', 'F2:B6:ED', 'F2:C7:80']
 numsensors = len(serialports)
 
@@ -81,6 +82,13 @@ connect_threads = {}
 quit_IMU = False
 passkey = "1234"  # passkey of shimmers
 pos = np.arange(len(CATEGORIES))
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+with open(f'{dir_path}/scale_params.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = np.array(list(reader))
+    means = data[1:, 1].astype(np.float)
+    scales = data[1:, -1].astype(np.float)
 
 if args.disp:
     plt.ion()
@@ -126,6 +134,10 @@ def plot_func(plotdata):
 
         plt.pause(0.0001)
 
+def scale_data(new_data):
+    new_data = (new_data-means)/scales
+    return new_data
+    
 
 class shimmer():
     def __init__(self, q):
@@ -285,7 +297,7 @@ class shimmer():
 
                 try:
                     self._connection = subprocess.Popen(f"sudo rfcomm connect {self._port} {target_address} 1", shell=True)
-                    time.sleep(1)
+                    time.sleep(2)
                     self._connect_error = False
                     return True
 
@@ -367,13 +379,17 @@ class shimmer():
         # Start thread for shimmer connection
         # connect_threads[num] = threading.Thread(target=shimmers[num].bt_connection, args=(num,))
         # connect_threads[num].start()
+        
         if self.bt_connection():
-            if self.initiate():  # Send set up commands, etc to shimmer
-                print(f'---Initiated {self._location} Sensor---')
-                return True
-            else:
-                print(f"Failed to initialise {self._location} sensor")
-                return False
+            count = 0
+            while count <= 3: 
+                if self.initiate():  # Send set up commands, etc to shimmer
+                    print(f'---Initiated {self._location} Sensor---')
+                    return True
+                else:
+                    count += 1
+                    print(f"Failed to initialise {self._location} sensor, attempt {count}/3")
+                    return False
         else:
             print(f"Failed to connect {self._location} sensor")
             return False
@@ -417,29 +433,38 @@ class shimmer():
     def shutdown(self):
         # Reset all flags/parameters
         self._connected = False
-        self._connection = None
+        #self._connection = None
         self._ready = False
         self._connect_error = True
 
         # Shut sensor down and kill serial connection
         try:
-            # send stop streaming command
-            self._serial.write(struct.pack('B', 0x20))
-            print(f"{self._location} stop command sent, waiting for ACK_COMMAND")
-            if self.wait_for_ack():
-                print(f"---{self._location} stop ACK_COMMAND received.")
-                # close serial port
-                self._serial.close()
-            else:
-                print(f"---{self._location} stop ACK_COMMAND *NOT* received.")
-        except Exception:
+            count = 0
+            sd = False
+            while (not sd) and (count <= 3):
+                # send stop streaming command
+                self._serial.write(struct.pack('B', 0x20))
+                print(f"{self._location} stop command sent, waiting for ACK_COMMAND")
+                if self.wait_for_ack():
+                    print(f"---{self._location} stop ACK_COMMAND received.")
+                    # close serial port
+                    self._serial.close()
+                    sd = True
+                else:
+                    count += 1
+                    #self._serial.close()
+                    print(f"---{self._location} stop ACK_COMMAND *NOT* received. Attempt {count}/3")
+        except Exception as e:
+            print(f"{self._location} close down sensor error: {e}")
             pass
 
         #Kill bluetooth connection
         try:
-            self._connection.release()
-            self._connection.kill()
-        except Exception:
+            #self._connection.release()
+            #self._connection.kill()
+            os.kill(self._connection.pid, 1)
+        except Exception as e:
+            print(f"{self._location} connection not killed: {e}")
             pass
 
         return True
@@ -530,11 +555,12 @@ def IMUsensorsMain():
             for p in shimmers:
                 new_data = np.hstack((new_data, shimmers[p]._accel, shimmers[p]._gyro))
             new_data = np.nan_to_num(new_data)
-            scaler = preprocessing.StandardScaler()
+            #scaler = preprocessing.StandardScaler()
             # for i in range(0, X.shape[0]):
             #     scaler = preprocessing.StandardScaler()
             #     X[i, :, :] = scaler.fit_transform(X[i, :, :])
-            new_data[:, :] = scaler.fit_transform(new_data[:, :])
+            #new_data[:, :] = scaler.fit_transform(new_data[:, :])
+            new_data = scale_data(new_data)
             status[3] = 1 # Ready
             diag_level = 0 # ok
 
@@ -557,13 +583,18 @@ def IMUsensorsMain():
         diag_msg = "Some helpful message"
         keyvalues = [KeyValue(key = f'Overall', value = IMU_SYS_MSGS[status[3]])]
 
-        act_obj.publish(prediction.tolist())
+        try:
+            act_obj.publish(prediction.tolist())
+        except Exception as e:
+            print(e)
+            print(prediction)
         diag_obj.publish(diag_level, diag_msg)
 
         rate.sleep()
 
 
 if __name__ == "__main__":
+    #subprocess.call("sudo service bluetooth restart")
     subprocess.call("sudo rfcomm release all", shell=True)
     # kill any rfcomm connections currently active
     subprocess.call("sudo killall rfcomm", shell=True)

sam_nodes/scripts/user.py
@@ -7,7 +7,7 @@ class User:
     def __init__(self, name, id):
         self._imu_pred = np.zeros(6) #class confs, t 
         self._imu_pred_hist = np.empty(6) #class confs, t 
-        self._imu_state_hist = np.array([4, 1, datetime.time.min, datetime.time.min]) #class, conf, tStart, tEnd
+        self._imu_state_hist = np.array([4, 1, datetime.datetime.min, datetime.datetime.min]) #class, conf, tStart, tEnd
         self.left_h_pos = [None, None, None]
         self.left_h_rot = [None, None, None, None]
         self.right_h_pos = [None, None, None]

sam_nodes/scripts/users_node.py
@@ -54,8 +54,9 @@ def current_action_callback(data, users):
         print(f"ERROR: users list name {users[data.UserId].name} does not match current_action msg name {data.UserName}")
     else:
         #users[data.UserId].actions.extend([data.action_probs, data.Header.stamp])
-        users[data.UserId]._imu_pred_hist = np.vstack((users[data.UserId]._imu_pred_hist, (np.hstack((data.ActionProbs, data.Header.stamp)))))
-        users[data.UserId]._imu_state_hist = np.vstack((users[data.UserId]._imu_state_hist, [np.argmax(data.ActionProbs).astype(float), 0, data.Header.stamp, data.Header.stamp]))
+        time = datetime.datetime.utcfromtimestamp(data.Header.stamp.to_sec())
+        users[data.UserId]._imu_pred_hist = np.vstack((users[data.UserId]._imu_pred_hist, (np.hstack((data.ActionProbs, time)))))
+        users[data.UserId]._imu_state_hist = np.vstack((users[data.UserId]._imu_state_hist, [np.argmax(data.ActionProbs).astype(float), 0, time, time]))
 
         users[data.UserId]._imu_state_hist, users[data.UserId]._imu_pred_hist = collate_imu_seq(users[data.UserId]._imu_state_hist, users[data.UserId]._imu_pred_hist)
 
@@ -79,7 +80,7 @@ def users_node():
     rate = rospy.Rate(1) # 1hz
 
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+        hello_str = f"{frame_id} active"
         rospy.loginfo(hello_str)
 
         diag_obj.publish(0, "Running")